baf6d0d36058aaf7d28db008d4af5507f58e3082
clf_programing <esx56487@qq.com>
2018-07-27 22:02:27 +0800

    初始化node服务器以及mysql
    已经可以用fetch提交post请求道服务器将博客内容save到DB，解决跨域问题，服务端和客户端都可以相互接收到批次发送的内容

--- server/index.js
+++ server/index.js
@@ -1,7 +1,54 @@
 var express = require('express');
+var db = require('./db');
 var app = express();
+var bodyParser = require('body-parser');
+var urlencodeParser = bodyParser.urlencoded({extended: false});
 app.use(express.static(__dirname + '/build'));
-app.get('/',(req,res)=>{
-    res.sendFile(__dirname + '/build/index.html');
+/**
+ * 设置跨域访问
+ */
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By",' 3.2.1');
+    res.header("Content-Type", "application/json;charset=utf-8");
+    next();
 });
-app.listen(3000);
+/**
+ * 返回打包后的bundle
+ */
+app.get('/',(req,res)=>{
+    //res.sendFile(__dirname + '/build/index.html');
+});
+/**
+ * 添加新博客到数据库
+ */
+app.post('/add_blog',urlencodeParser,(req,res)=>{
+    let params = [
+        req.body.title,
+        req.body.content,
+        req.body.label,
+        req.body.type,
+        req.body.classify,
+        req.body.isPrivate,
+        req.body.createTime,
+        req.body.isPublish
+    ];
+    let addBlog = 'INSERT blogs(title,content,label,type,classify,isPrivate,createTime,isPublish) VALUES(?,?,?,?,?,?,?,?)';
+    db.DBConnection.query(addBlog,params,(err,rows,fields)=>{
+        if(err){
+            console.log(err);
+            return;
+        }
+        let success = {
+            message: '新建博客成功！'
+        };
+        console.log('success');
+        res.send(success);
+    });
+});
+/**
+ * 监听9000端口
+ */
+app.listen(9000);
