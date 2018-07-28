d87cbbfb09dd4b0b45c86e4fb71ac9a5bfca7666
clf_programing <esx56487@qq.com>
2018-07-28 10:18:16 +0800

    完成redux配置和使用
    数据交互全交给redux

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import RouterConfig from './router/index';
 import registerServiceWorker from './registerServiceWorker';
+import {Provider} from 'react-redux';
+import {store} from './store';
 
-ReactDOM.render(<RouterConfig />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <RouterConfig />
+    </Provider>,
+    document.getElementById('root'));
 registerServiceWorker();
--- src/pages/BackstageManagement.jsx
+++ src/pages/BackstageManagement.jsx
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
-import {Layout,Menu,Icon,Button} from 'antd';
+import {Layout} from 'antd';
 import '../css/pages/BackstageManagement.css';
 import SiderComponent from '../components/SiderComponent';
 import HeaderComponent from '../components/HeaderComponent';
 import Editor from '../components/Editor';
-const {Header,Content,Footer,Sider} = Layout;
-const SubMenu = Menu.SubMenu;
+const {Header,Content,Sider} = Layout;
 class BackstageManagement extends Component {
   render() {
     return (
